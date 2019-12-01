fbb6f40f2f11115bf3d66656e18fec3ccec78157
Diego Gomez <diego.f.gomez.pardo@gmail.com>
2019-12-01 16:42:16 -0500

    fix strong parameters

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,18 +1,18 @@
 class GamesController < ActionController::API
   def create
-    service = StarGame.new
-    service.call(create_params)
+    service = StartGame.new
+    service.call(names: create_params)
 
     if service.valid?
       render json: "Let's play guys!!"
     else
-      render json: { error: service.error }
+      render json: { error: service.errors.full_messages[0] }
     end
   end
 
   private
 
   def create_params
-    params.require(:names).permit(:names)
+    params.permit(names: []).require(:names)
   end
 end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,12 +1,12 @@
 describe GamesController, type: :request do
   let(:names) do
-    3.times { |i| "Alan #{i}" }
+    3.times.map { |i| "Alan #{i}" }
   end
 
   context 'POST - Create Game' do
     it 'should initialize a game' do
       post games_path, params: { names: names }
-      expect(response.body).to be "let's play guys!!"
+      expect(response.body).to eq "Let's play guys!!"
     end
   end
 end
